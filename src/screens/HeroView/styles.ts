acc87bbe9da7a0ba733cf7dabfec19024dde59f6
Nick Lvov <nick.lvov.dev@yandex.ru>
2020-03-12 20:00:25 +0700

    draw next implementation

--- src/screens/HeroView/styles.ts
+++ src/screens/HeroView/styles.ts
@@ -7,18 +7,29 @@ export default StyleSheet.create({
     alignItems: 'center',
   },
   draw: {
-    overflow: 'hidden',
     borderRadius: 32,
     height: 160,
     width: 240,
     marginVertical: 32,
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'relative',
   },
   modifier: {
     borderRadius: 32,
     overflow: 'hidden',
     width: 240,
     height: 160,
+  },
+  drawnModifierWrapper: {
+    position: 'absolute',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
 });
