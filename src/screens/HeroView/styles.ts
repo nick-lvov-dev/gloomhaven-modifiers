e616bff1180e6c32d216f6c29bc6f67bd0df6c21
Nick Lvov <nick.lvov.dev@yandex.ru>
2020-04-04 15:05:54 +0700

    hero view layout refactor

--- src/screens/HeroView/styles.ts
+++ src/screens/HeroView/styles.ts
@@ -15,12 +15,8 @@ export default StyleSheet.create({
   delete: { height: 24, width: 24, resizeMode: 'contain', transform: [{ rotate: '45deg' }] },
   edit: { height: 24, width: 24, resizeMode: 'contain' },
   shuffleWrapper: {
-    backgroundColor: colors.darker,
-    padding: 8,
-    paddingLeft: 9,
-    paddingRight: 7,
-    borderRadius: 100,
-    marginBottom: 8
+    alignSelf: 'flex-start',
+    transform: [{ rotate: '45deg' }],
   },
   actions: { position: 'absolute', right: 0, top: 0, padding: 16, alignItems: 'center' },
   modifiers: { position: 'absolute', left: 0, top: 0, padding: 16, alignItems: 'stretch' },
@@ -29,7 +25,8 @@ export default StyleSheet.create({
   actionShadow: { position: 'absolute', top: 2, width: 42, height: 42, resizeMode: 'contain', zIndex: -1, tintColor: '#000000aa' },
   modifierAction: { width: 40, height: 40, resizeMode: 'contain' },
   modifierActionText: { fontFamily: FontFamily.Pirata, fontSize: 32, color: colors.dark },
-  advantageDisadvantage: { width: 60, height: 55, resizeMode: 'contain' },
+  advantageDisadvantage: { width: 60, height: 60, resizeMode: 'contain' },
+  advantageDisadvantageWrapper: { position: 'absolute', left: 16, top: '50%', transform: [{ translateY: -30 }] },
   shuffle: { width: 24, height: 24, resizeMode: 'contain' },
   total: {
     color: '#000',
@@ -38,5 +35,5 @@ export default StyleSheet.create({
     alignSelf: 'stretch',
     textAlign: 'center',
   },
-  deckContainer: { flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' },
+  deckContainer: { flexDirection: 'row', justifyContent: 'center', alignItems: 'center' },
 });
